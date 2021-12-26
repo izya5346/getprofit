ce7f7b5281c4950bed9076fb3164c1e183c20466
izya5346 <karimov2203@gmail.com>
2021-12-26 14:48:38 +0300

    back fix

--- main.py
+++ main.py
@@ -96,6 +96,9 @@ async def deling(call: types.CallbackQuery):
 async def deleted(call: types.CallbackQuery, callback_data: dict):
     models.Item.delete_by_id(int(callback_data['id']))
     await bot.send_message(call.from_user.id, 'Успешно удалено', reply_markup = mainmenu)
+@dp.callback_query_handler(text='del_item') 
+async def backed(call: types.CallbackQuery):
+    await bot.send_message(call.from_user.id, 'Здарова пидрила', reply_markup=mainmenu)
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
